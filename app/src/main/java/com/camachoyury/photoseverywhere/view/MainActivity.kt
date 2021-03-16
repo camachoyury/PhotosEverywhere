1f5c4c0f2b5b507ee982c71e9c727055111e7708
Yury Camacho <yury.camacho@rappi.com>
2021-03-16 10:18:21 -0400

    added toolbar

--- app/src/main/java/com/camachoyury/photoseverywhere/view/MainActivity.kt
+++ app/src/main/java/com/camachoyury/photoseverywhere/view/MainActivity.kt
@@ -4,11 +4,17 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import com.camachoyury.photoseverywhere.view.ui.theme.PhotosTheme
 import com.camachoyury.photoseverywhere.viewmodel.PhotosViewModel
@@ -16,6 +22,7 @@ import com.camachoyury.photoseverywhere.viewmodel.ScreenState
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collect
+import com.camachoyury.photoseverywhere.R
 
 
 @AndroidEntryPoint
@@ -28,12 +35,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent{
+        setContent {
             PhotosTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("YURY")
-                }
+//                // A surface container using the 'background' color from the theme
+//                Surface(color = MaterialTheme.colors.background) {
+//                    Greeting("YURY")
+//                }
+                Scaffold (
+                    topBar = {ToolBar()},
+                    content = {
+                        Greeting("YURY")
+                    }
+
+                        )
             }
         }
 
@@ -54,6 +68,9 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+
+
+
 @Composable
 fun Greeting(name: String) {
 
@@ -65,7 +82,42 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     PhotosTheme {
-       Greeting(name = "YURY CAMACHO")
+        Greeting(name = "YURY CAMACHO")
+        ToolBar()
 
     }
 }
+
+
+@Composable
+fun ToolBar() {
+
+
+    TopAppBar(
+        modifier = Modifier.padding(3.dp),
+        navigationIcon = {
+            Icon(
+                modifier = Modifier.padding(start = 8.dp),
+                painter = painterResource(id = R.drawable.ic_baseline_message_24),
+                contentDescription = null // decorative element
+            )
+        },
+        actions = {
+            Surface(
+                modifier = Modifier.size(50.dp),
+                shape = CircleShape,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.jetpack_logo),
+                    contentDescription = ""
+                )
+            }
+        },
+        title = { Text(text = "Google Photos") },
+        elevation = 16.dp
+
+    )
+}
+
+
