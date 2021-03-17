461545413d353f673fd2a66d3e278e6abb091ad7
Yury Camacho <yury.camacho@rappi.com>
2021-03-17 19:41:25 -0400

    Added Rotation behavior

--- app/src/main/java/com/camachoyury/photoseverywhere/view/MainActivity.kt
+++ app/src/main/java/com/camachoyury/photoseverywhere/view/MainActivity.kt
@@ -3,17 +3,30 @@ package com.camachoyury.photoseverywhere.view
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.StarRate
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
 import com.camachoyury.photoseverywhere.view.ui.theme.PhotosTheme
@@ -23,7 +36,15 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collect
 import com.camachoyury.photoseverywhere.R
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import kotlin.math.roundToInt
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -40,13 +61,11 @@ class MainActivity : AppCompatActivity() {
                 Scaffold(
                     topBar = { ToolBar() },
                     content = {
-                        Greeting("YURY")
+                        Drag(imageId = R.drawable.dog1)
                     }
-
                 )
             }
         }
-
 
         viewModel.load()
         lifecycleScope.launchWhenStarted {
@@ -64,28 +83,16 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-
-@Composable
-fun Greeting(name: String) {
-
-    Text(text = "Hello $name!")
-}
-
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     PhotosTheme {
-        Greeting(name = "YURY CAMACHO")
-        ToolBar()
-
+        Drag(imageId = R.drawable.dog1)
     }
 }
 
-
 @Composable
 fun ToolBar() {
-
     TopAppBar(
         navigationIcon = {
             Icon(
@@ -106,9 +113,105 @@ fun ToolBar() {
                 )
             }
         },
-        title = { Text(text = "Google Photos") },
+        title = {
+            Text(
+                text = "Google Photos"
+            )
+                },
         elevation = 16.dp
     )
 }
+
+@Composable
+fun Drag(@DrawableRes imageId: Int) {
+
+    Box(modifier = Modifier.background(Color.White).padding(8.dp).fillMaxSize()) {
+        var offsetX by remember { mutableStateOf(0f) }
+        var offsetY by remember { mutableStateOf(0f) }
+        val scale by remember { mutableStateOf(1f) }
+        var rotation by remember { mutableStateOf(0f) }
+        var offset by remember { mutableStateOf(  Offset.Zero) }
+
+        Card(
+            Modifier
+                .fillMaxSize()
+                .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+                .background(Color.White)
+                .size(200.dp, 300.dp)
+                .graphicsLayer(
+                    scaleX = scale,
+                    scaleY = scale,
+                    rotationZ = rotation,
+                    translationX = offset.x,
+                    translationY = offset.y
+                )
+                .pointerInput(Unit) {
+                    detectDragGestures(onDragEnd = {
+                        rotation =  0f
+                        offsetX = scale
+                        offsetY = scale
+                    }) { change, dragAmount ->
+                        change.consumeAllChanges()
+                        rotation = if (offsetX > 0){
+                            -20f
+                        }else 20f
+                        offsetX = offsetX.plus(dragAmount.x)
+                        offsetY = offsetY.plus(dragAmount.y)
+//
+
+                    }
+                },
+            backgroundColor= Color.White,
+            border = BorderStroke(1.dp, Color.Black),
+            shape = RoundedCornerShape(12.dp)
+        ) {
+            Image(
+                contentScale = ContentScale.FillBounds,
+                painter = painterResource(id = imageId),
+                contentDescription = ""
+            )
+
+            Row(
+                Modifier.wrapContentHeight(align = Alignment.Bottom).background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color.White.copy(alpha = 0.1f),
+                            Color.Black
+                        )
+                    )),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.Bottom
+            ) {
+                Button(Icons.Rounded.Close,
+                    tint = Color.Red)
+                Button(Icons.Rounded.StarRate,
+                    tint = Color.Blue)
+                Button( Icons.Rounded.Favorite,
+                    tint = Color.Green)
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Button(icon: ImageVector, tint: Color ){
+
+    Surface(
+        modifier = Modifier.padding(12.dp).size(54.dp),
+        border = BorderStroke(2.dp, tint),
+        shape = CircleShape,
+        color = Color.White.copy(alpha = 0.1f)
+    ) {
+        Icon(
+            icon,
+            modifier=Modifier.padding(4.dp).size(24.dp),
+            tint = tint,
+            contentDescription = "Localized description")
+    }
+}
+
+
+
 
 
